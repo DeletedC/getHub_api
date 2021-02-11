41ab070f5f70f57dc08ed56a17d38ba63a9ffd5a
DeletedC <CurtisWoodsArt@gmail.com>
2021-02-11 12:49:06 -0500

    Setup basic Express server

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const cors = require('cors');
+require('dotenv').config();
+
+///////////////////////////////////////
+// CORS CONFIGURATION
+///////////////////////////////////////
+
+// List of urls our API will accept calls from
+const whitelist = [];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+///////////////////////////////////////
+// MIDDLEWARE
+///////////////////////////////////////
+app.use(cors(corsOptions));
+
+///////////////////////////////////////
+// ROUTES
+///////////////////////////////////////
+
+// INDEX ROUTE
+app.get('/', (req, res) => {
+    res.send("Welcome to getHub API!");
+});
+
+
+///////////////////////////////////////
+// LISTENING
+///////////////////////////////////////
+
+app.listen(PORT, () => {
+    console.log(`Port: ${PORT}`); 
+    console.log("Hello, Seattle. I'm listening...");
+});
