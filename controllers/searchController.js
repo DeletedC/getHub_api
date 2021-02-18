847b3e7399d979baf8dedb77092740fe6cc201d3
DeletedC <CurtisWoodsArt@gmail.com>
2021-02-18 14:36:19 -0500

    Working on search scraping function

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -9,9 +9,13 @@ const puppeteer = require('puppeteer');
 ///////////////////////////////////////
 
 searchRouter.post('/', async (req, res) => {
+
+    const { query } = req.body;
+    const finalResult = {};
+
+    console.log(`Post request received. Searching for \n"${query}"`);
 //     // OMDB
 //     try {
-//         const { query } = req.body;
 //         console.log(`Query from front is ${query}`)
 //         const result = await axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&s=${query}`);
 //         console.log('Results from TRY block')
@@ -22,57 +26,100 @@ searchRouter.post('/', async (req, res) => {
 //         res.send('Sorry, the request failed.');
 //     }
 
+// AMAZON PRIME SEARCH
+const amazonSearch = async () => {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+
+    await page.goto('https://www.amazon.com/s?k=sonic&i=instant-video')
+        .then(() => console.log(`Initiated Amazon Prime search for ${query}`));
+
+    await page.waitForSelector('.s-result-item', {visible: true, timeout: 3000})
+        .then(() => console.log('Waited for 3 seconds.'))
+        .then(async () => {
+            const data = await page.evaluate(() => {
+                const results = document.querySelectorAll('.s-result-item');
+                console.log('Results:');
+                console.log(results);
+
+                // Using this to find the data I need
+                const temp = Array.from(results).map(item => {
+                    item.innerHTML;
+                    console.log(item);
+                });
+                // console.log(temp);
+
+                return temp;
+            })
+
+            // console.log(data);
+            res.send('done');
+        })
+        .catch(error => {
+            console.log('Error inside the scraper function.');
+            console.log(error);
+            res.send(`Error inside the scraper function. \n${error}`);
+        })
+}
+    try {
+        amazonSearch();
+    } catch (error) {
+        console.log('Amazon Search failed.');
+        console.log(error);
+        res.send('Amazon search failed.');
+    }
 
 });
 
-searchRouter.get('/', async (req, res) => {
-    // AMAZON 
-    let data;
-    const scrapeMovies = async () => {
+// AMAZON PRIME BROWSER
+// searchRouter.get('/', async (req, res) => {
+//     // AMAZON 
+//     let data;
+//     const scrapeMovies = async () => {
 
-        const browser = await puppeteer.launch({headless: true});
-        const page = await browser.newPage();
+//         const browser = await puppeteer.launch({headless: true});
+//         const page = await browser.newPage();
         
-        // Make the viewport vertically huge to load more data at once
-        await page.setViewport({
-            width: 640,
-            height: 10000
-        });
+//         // Make the viewport vertically huge to load more data at once
+//         await page.setViewport({
+//             width: 640,
+//             height: 10000
+//         });
 
-        await page.goto('https://www.amazon.com/gp/video/storefront/')
-            .then(() => console.log('Went to Amazon Prime Storefront.'));
+//         await page.goto('https://www.amazon.com/gp/video/storefront/')
+//             .then(() => console.log('Went to Amazon Prime Storefront.'));
 
-        // While the load more button exists, click it.
-        // When it's gone, pull the data from the dom.
+//         // While the load more button exists, click it.
+//         // When it's gone, pull the data from the dom.
 
-        // while (document.querySelector('[aria-label="Load more videos]') == true) {
+//         // while (document.querySelector('[aria-label="Load more videos]') == true) {
             
-            // FILL IN LOOP LOGIC HERE
+//             // FILL IN LOOP LOGIC HERE
 
-        // }
-        await page.waitForSelector('.hC3fkr', {visible: true})
-            .then(() => console.log('Waited for selector.'))
-            .then(async () => {
-                data = await page.evaluate(() => {
-                    const movies = document.querySelectorAll('.hC3fkr');
+//         // }
+//         await page.waitForSelector('.hC3fkr', {visible: true})
+//             .then(() => console.log('Waited for selector.'))
+//             .then(async () => {
+//                 data = await page.evaluate(() => {
+//                     const movies = document.querySelectorAll('.hC3fkr');
         
-                    const urls = Array.from(movies).map(item => item.innerHTML);
+//                     const urls = Array.from(movies).map(item => item.innerHTML);
         
-                    return urls;
-                })
-                console.log(data);
-            })
-            .then(async () => {
-                await browser.close();
-            });
-    }
+//                     return urls;
+//                 })
+//                 console.log(data);
+//             })
+//             .then(async () => {
+//                 await browser.close();
+//             });
+//     }
 
-    await scrapeMovies()
-        .then(() => {
-            res.send(data);
-        });
+//     await scrapeMovies()
+//         .then(() => {
+//             res.send(data);
+//         });
 
-})
+// })
 
 
 module.exports = searchRouter;
