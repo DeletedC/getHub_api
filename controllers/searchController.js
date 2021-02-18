a2c7309fda3c553eefa8f1298526e6f5fe6fa2aa
DeletedC <CurtisWoodsArt@gmail.com>
2021-02-18 12:50:51 -0500

    Started work on Amazon search api

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -8,48 +8,48 @@ const {JSDOM} = jsdom;
 ///////////////////////////////////////
 
 searchRouter.post('/', async (req, res) => {
-//     // OMDB
-//     try {
-//         const { query } = req.body;
-//         console.log(`Query from front is ${query}`)
-//         const result = await axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&s=${query}`);
-//         console.log('Results from TRY block')
-//         console.log(result.data.Search);
-//         res.send(result.data.Search);
-//     } catch (error) {
-//         console.log(error);
-//         res.send('Sorry, the request failed.');
-//     }
+    // OMDB
+    try {
+        const { query } = req.body;
+        console.log(`Query from front is ${query}`)
+        const result = await axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&s=${query}`);
+        console.log('Results from TRY block')
+        console.log(result.data.Search);
+        res.send(result.data.Search);
+    } catch (error) {
+        console.log(error);
+        res.send('Sorry, the request failed.');
+    }
 
 
 });
 
-searchRouter.get('/', async (req, res) => {
-    // AMAZON 
+// searchRouter.get('/', async (req, res) => {
+//     // AMAZON 
 
-    let amazonData = [];
-    try {
-        await axios.get('https://www.curtisjwoods.com')
-            .then((data) => {
-                // console.log(data.data);
-                const dom = new JSDOM(data.data);
-                // console.log(dom);
-                // console.log(dom.window.document.querySelectorAll('.card-imae'));
-                dom.window.document.querySelectorAll('.card-image > img').forEach(item => {
-                    console.log(item.getAttribute('src'));
-                    amazonData.push(item.getAttribute('src'));
-                    console.log(amazonData);
-                })
-                // console.log(dom.window.document);
-                res.send(amazonData);
-            })
-    } catch (error) {
-        console.log('I blew up.');
-        console.log(error);
-        res.send('I blew up.');
-    }
+//     let amazonData = [];
+//     try {
+//         await axios.get('https://www.curtisjwoods.com')
+//             .then((data) => {
+//                 // console.log(data.data);
+//                 const dom = new JSDOM(data.data);
+//                 // console.log(dom);
+//                 // console.log(dom.window.document.querySelectorAll('.card-imae'));
+//                 dom.window.document.querySelectorAll('.card-image > img').forEach(item => {
+//                     console.log(item.getAttribute('src'));
+//                     amazonData.push(item.getAttribute('src'));
+//                     console.log(amazonData);
+//                 })
+//                 // console.log(dom.window.document);
+//                 res.send(amazonData);
+//             })
+//     } catch (error) {
+//         console.log('I blew up.');
+//         console.log(error);
+//         res.send('I blew up.');
+//     }
 
-})
+// })
 
 
 module.exports = searchRouter;
