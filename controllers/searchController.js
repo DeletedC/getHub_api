e66336dcc60049964bb1b694dd6f0e7277e5bc5d
DeletedC <CurtisWoodsArt@gmail.com>
2021-04-05 15:35:21 -0400

    Added logic for invalid queries and a catch function

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -10,18 +10,34 @@ searchRouter.post('/', async (req, res) => {
     try {
         const { query } = req.body;
         const queryString = query.toString();
-        
-        const result = await axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&s=${queryString}`);
 
-        if (result.data.Response == 'False') {
-            res.send('no-results');
-        } else {
-            console.log(result.data.Search);
-            console.log(`queryString is ${queryString}`);
+        await axios.get(`http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY}&s=${queryString}`)
+            .then((data) => {
+                const result = data;
+                // if the proper data exists and the query didn't explode
+                console.log(result)
+                if (result.data.Response) {
 
-            res.send(result.data.Search);
-        }
-        
+                    // Check to see if the response is 'False' (meaning no results)
+                    // 'False' and 'True' in this data are strings, not boolean
+                    if (result.data.Response == 'False') {
+                        res.send('no-results');
+                    } else {
+                        console.log(result.data.Search);
+                        console.log(`queryString is ${queryString}`);
+            
+                        res.send(result.data.Search);
+                    }
+                }
+            })
+            // If the request exploded or is invalid, send back 'no-results'
+            .catch((error) => {
+                console.log(`queryString is ${queryString}`);
+                console.log(`Error: ${error.response.status} ${error.response.statusText}`);
+
+                res.send('no-results');
+            });
+   
     } catch (error) {
         console.log(error);
         res.send('Sorry, the request failed.');
